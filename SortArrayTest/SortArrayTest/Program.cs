4c26028975b39182fbb8d2dd556557cb08059303
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-08 13:45:06 +0300

    Add files via upload

---
+++ SortArrayTest/SortArrayTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortArrayLibrary;
+namespace SortArrayTest
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[] x = new int[10];
+            int[] y = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < x.Length; i++) {
+                
+                x[i] = random.Next(1, 100);
+               y[i] = random.Next(1, 100);
+            }
+            Console.WriteLine(" Не упорядоченный массив для быстрой сортировки: {0}", string.Join(", ", x));
+            Console.WriteLine(" Не упорядоченный массив для сортировки слиянием: {0}", string.Join(", ", y));
+            
+            Console.WriteLine("Упорядоченный массив(быстрая): {0}", string.Join(", ", Sort.QuickSort(x)));
+            Console.WriteLine("Упорядоченный массив(слияние): {0}", string.Join(", ", Sort.MergeSort(y)));
+           
+        }
+    }
+    }
+
